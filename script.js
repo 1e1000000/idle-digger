ff7a0c45090295d846cade0b9205bbd0247fd19b
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-26 14:53:33 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ game = {
   num: 0
 }
 
+let deltaTime;
+const calculate = window.setInterval(() => {
+  deltaTime = Date.now() - game.lastTick;
+  loop(deltaTime);
+}, game.msint);
+
 function loop(unadjusted, off = 0) {
   unadjusted=50
   get("number").textContent = "Your number is currently "+ game.num
