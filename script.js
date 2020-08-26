5b614db18e2bdff5010c667a101d0308c176db12
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-26 17:59:30 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 game = {
-  lastTick: 0,
-  num: 0
+  num: 0,
+  lastTick: 0
 }
 
 let deltaTime;
 const calculate = window.setInterval(() => {
   deltaTime = Date.now() - game.lastTick;
-  loop(deltaTime);
-  game.lastTick = Date.now()
+  game.lastTick = Date.now();
+  loop(deltaTime)
 }, game.msint);
 
 function loop(unadjusted, off = 0) {
