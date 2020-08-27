30c4840fbcf25906617bfeee298827c841c0c01d
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-27 11:31:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const calculate = window.setInterval(() => {
   deltaTime = Date.now() - game.lastTick;
   loop(deltaTime);
   game.lastTick = Date.now()
-}, game.msint);
+}, 0);
 
 function loop(unadjusted, off = 0) {
   game.num += unadjusted/1000
