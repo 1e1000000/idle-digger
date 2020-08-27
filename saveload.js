069166b2ebff93aabe07c1379490bfb77b5e4de1
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-27 11:30:07 +0800

    save test

--- saveload.js
+++ saveload.js
@@ -1,4 +1,12 @@
-let game = {
+function reset() {
+  game = {
   num: 0,
   lastTick: Date.now()
+  }
+}
+
+function save() {
+  if (AF === 0) {
+    localStorage.setItem("inc", JSON.stringify(game))
+  };
 }
