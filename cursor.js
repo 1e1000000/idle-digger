201ea7b140e4cb6bf98d107e002c71c6c9ffa72a
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-28 18:22:00 +0800

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,1 +1,4 @@
-
+function getCursorCost(amount) { // amount: currently cursors
+  if (amount.lt(10)) return new Decimal(10).add(amount)
+  return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10)))
+}
