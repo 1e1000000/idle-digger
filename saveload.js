802f66fe53b416006f02920957c642b212997416
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-28 17:10:00 +0800

    Update saveload.js

--- saveload.js
+++ saveload.js
@@ -77,6 +77,27 @@ function copyStringToClipboard(str) {
   document.body.removeChild(el);
 }
 
+function copyToClipboard(el) {
+  el = typeof el === "string" ? document.querySelector(el) : el;
+  if (navigator.userAgent.match(/ipad|ipod|iphone/i)) {
+    var editable = el.contentEditable;
+    var readOnly = el.readOnly;
+    el.contentEditable = true;
+    el.readOnly = true;
+    var range = document.createRange();
+    range.selectNodeContents(el);
+    var selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    el.setSelectionRange(0, 999999);
+    el.contentEditable = editable;
+    el.readOnly = readOnly;
+  } else {
+    el.select();
+  }
+  document.execCommand("copy");
+}
+
 function importSave(text) {
   savegame = JSON.parse(atob(text));
   objectToDecimal(savegame);
