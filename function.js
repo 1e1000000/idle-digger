e8a7d486577e5411b8892dd0c03890e71c2e599c
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-28 11:13:34 +0800

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,12 @@ function damage() {
   game.dealed = (game.dealed).add(0.01)
 }
 
-function getDepth(ret) {
+function getDepth(ret) { // ret: dealed
   if (ret.lt(1)) return new Decimal(0)
   return new Decimal(10).pow(ret.log10().sqrt()).floor().max(0)
+}
+
+function getHealth(ret) { // ret: depth
+  if (ret.lt(0)) return 0
+  return new Decimal(10).pow(ret.add(1).log10().pow(2))
 }
