34e7c310713e6e23046398298231ad568949b34f
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-28 11:07:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const calculate = window.setInterval(() => {
 
 function loop(unadjusted, off = 0) {
   game.dealed = (game.dealed).add(unadjusted/1000*0);
-  game.depth = getDepth();
+  game.depth = getDepth(game.dealed);
   document.getElementById("depth").innerHTML = "Your depth is currently " + game.depth.toFixed(0) + " meter"
   document.getElementById("health").innerHTML = "Your health on this block is currently " + new Decimal(1).sub(game.dealed).toFixed(2)
 }
