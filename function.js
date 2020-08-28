39c8635fddb080d6c04c10eca7970d7b709fe5a3
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-28 11:22:48 +0800

    Update function.js

--- function.js
+++ function.js
@@ -11,3 +11,7 @@ function getHealth(ret) { // ret: depth
   if (ret.lt(0)) return 0
   return new Decimal(10).pow(ret.add(1).log10().pow(2))
 }
+
+function getCoinPerSecond() {
+  return new Decimal(2).pow(game.depth.root(3)).sub(1)
+}
