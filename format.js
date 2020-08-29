40e48cedca2549523d2d2c3604b4493042030560
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-29 17:14:52 +0800

    Update format.js

--- format.js
+++ format.js
@@ -1,26 +1,9 @@
 function formate(ret, dp) {
   if (ret.lt(1e6)) {
     return ret.toFixed(dp)
-  } else if (ret.lt("(10^)^5 6")) {
-    let iterateLayer = 0; // how much e will be show before first number
-    let output = "";
-    if (ret.gte("eee1e6")) {
-      ret = ret.log10();
-      iterateLayer++
-    };
-    if (ret.gte("ee1e6")) {
-      ret = ret.log10();
-      iterateLayer++
-    };
-    if (ret.gte("e1e6")) {
-      ret = ret.log10();
-      iterateLayer++
-    };
+  } else if (ret.lt("e1e6")) {
     let exponent = ret.log10().floor();
     let mantissa = ret.div(new Decimal(10).pow(exponent));
-    for (let i=0; i<iterateLayer; i++) {
-      output = output + "e"
-    };
-    return output + mantissa.toFixed(dp) + "e" + exponent
+    return mantissa.toFixed(dp) + "e" + exponent
   } else return ret.toString()
 }
