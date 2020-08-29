9accf450aa052b475654489d967bc01494d04477
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-29 13:10:16 +0800

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -1,4 +1,12 @@
-function getCursorCost(amount) { // amount: currently cursors
+function getCursorCost(amount) { // amount: currently cursor
   if (amount.lt(10)) return new Decimal(10).add(amount)
   return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10)))
+}
+
+function buyCursor() {
+  if (game.coins.gte(getCursorCost(game.cursor.bought)))) {
+    game.coins = game.coins.sub(getCursorCost(game.cursor.bought))
+    game.cursor.amount = game.cursor.amount.add(1)
+    game.cursor.bought = game.cursor.bought.add(1)
+  }
 }
