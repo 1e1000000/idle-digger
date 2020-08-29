2019fdd52256319711be15f8553d849fe9fe39c7
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-29 14:56:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -34,4 +34,7 @@ function loop(unadjusted, off = 0) {
   document.getElementById("health").innerHTML = "Your health on this block is currently " + getHealth(game.depth).sub(game.dealed).toFixed(2) + "/" + getHealth(game.depth).sub(getHealth(game.depth.sub(1))).toFixed(2)
   document.getElementById("coins").innerHTML = "You have " + game.coins.toFixed(2) + " coins (+" + getCoinPerSecond().toFixed(2) + "/s)"
   document.getElementById("damage").innerHTML = "Deal Damage by " + getCursorDamage().toFixed(2)
+  document.getElementById("cursorAmount").innerHTML = "Cursor: " + game.cursor.amount.floor() + " (" + game.cursor.bought + " Bought)"
+  document.getElementById("cursorPower").innerHTML = "Power: 1x"
+  document.getElementById("cursorCost").innerHTML = "Cost: " + getCursorCost(game.cursor.bought)
 }
