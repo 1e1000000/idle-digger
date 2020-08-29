f061b8c4f608f8d9cfc31ed12eb81fd7a7f5694d
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-29 14:15:16 +0800

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -4,7 +4,7 @@ function getCursorCost(amount) { // amount: currently cursor
 }
 
 function buyCursor() {
-  if (game.coins.gte(getCursorCost(game.cursor.bought)))) {
+  if (game.coins.gte(getCursorCost(game.cursor.bought))) {
     game.coins = game.coins.sub(getCursorCost(game.cursor.bought))
     game.cursor.amount = game.cursor.amount.add(1)
     game.cursor.bought = game.cursor.bought.add(1)
@@ -13,4 +13,8 @@ function buyCursor() {
 
 function getCursorDamage() {
   return game.cursor.amount.add(1).div(100)
+}
+
+function damage() {
+  game.dealed = (game.dealed).add(getCursorDamage())
 }
