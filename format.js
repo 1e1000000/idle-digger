1f151093b4e087064ecf95240a77c612b6d8cc10
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-29 17:05:15 +0800

    Update format.js

--- format.js
+++ format.js
@@ -4,15 +4,15 @@ function formate(ret, dp) {
   } else if (ret.lt("(10^)^5 6")) {
     let iterateLayer = 0; // how much e will be show before first number
     let output = "";
-    if ret.gte("eee1e6") {
+    if (ret.gte("eee1e6")) {
       ret = ret.log10();
       iterateLayer++
     };
-    if ret.gte("ee1e6") {
+    if (ret.gte("ee1e6")) {
       ret = ret.log10();
       iterateLayer++
     };
-    if ret.gte("e1e6") {
+    if (ret.gte("e1e6")) {
       ret = ret.log10();
       iterateLayer++
     };
