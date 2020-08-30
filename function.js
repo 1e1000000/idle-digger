34d4f98fa8f37ac7ec96e668175f324a68c9934f
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-30 14:36:02 +0800

    Update function.js

--- function.js
+++ function.js
@@ -11,3 +11,10 @@ function getHealth(ret) { // ret: depth
 function getCoinPerSecond() {
   return new Decimal(2).pow(game.depth.root(3)).sub(1)
 }
+
+function toggleMaxBulk() {
+  let ret = prompt("Please type in the new max bulk buy (Note: higher bulk buy will cost more performance), min: 1, max: 10000");
+  if (ret < 1) game.maxBulk = 1
+  if (ret > 10000) game.maxBulk = 10000
+  game.maxBulk = Math.round(ret)
+}
