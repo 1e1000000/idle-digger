74828229d4ae97877d14de9bc7300ad6478e7789
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-30 16:44:15 +0800

    Update miner.js

--- miner.js
+++ miner.js
@@ -1,1 +1,5 @@
-
+function getMinerCost(generation,amount) { // generation: Miner #, amount: currently Miner # bought
+  if (amount.lt(1000)) return new Decimal(1000).mul(new Decimal(1.09).pow(amount))
+  if (amount.lt(10000)) return new Decimal(1000).mul(new Decimal(1.09).pow(amount.pow(2).div(1000)))
+  return new Decimal(1000).mul(new Decimal(1.09).pow(amount.pow(amount.log10().sqrt()).div(1000)))
+}
