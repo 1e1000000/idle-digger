4c4e4aeb246766c9950449bb6eeffa825779a7c7
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-30 11:05:26 +0800

    scaled and superscaled test

--- cursor.js
+++ cursor.js
@@ -1,6 +1,8 @@
 function getCursorCost(amount) { // amount: currently cursor
   if (amount.lt(10)) return new Decimal(10).add(amount)
-  return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10)))
+  if (amount.lt(1000)) return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10)))
+  if (amount.lt(10000)) return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10).pow(2).div(990)))
+  return new Decimal(20).mul(new Decimal(1.05).pow(amount.sub(10).pow(amount.sub(10).pow(amount.log10().sqrt())).div(990)))
 }
 
 function buyCursor() {
