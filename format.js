eb0ebe9650e6f24fa960e7931ace5d79b94b2a62
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-31 16:12:21 +0800

    Update format.js

--- format.js
+++ format.js
@@ -2,8 +2,21 @@ function formate(ret, dp) {
   if (ret.lt(1e6)) {
     return ret.toFixed(dp)
   } else if (ret.lt("e1e6")) {
-    let exponent = ret.log10().floor();
-    let mantissa = ret.div(new Decimal(10).pow(exponent));
-    return mantissa.toFixed(2) + "e" + exponent
+    return formateScientific(ret, 2)
+  } else if (ret.lt("ee1e6")) {
+    ret = ret.log10()
+    return "e" + formateScientific(ret, 3)
+  } else if (ret.lt("eee1e6")) {
+    ret = ret.log10().log10()
+    return "ee" + formateScientific(ret, 3)
+  } else if (ret.lt("eeee1e6")) {
+    ret = ret.log10().log10().log10()
+    return "eee" + formateScientific(ret, 3)
   } else return ret.toString()
+}
+
+function formateScientific(ret, dp) {
+  let exponent = ret.log10().floor();
+  let mantissa = ret.div(new Decimal(10).pow(exponent));
+  return mantissa.toFixed(dp) + "e" + exponent
 }
