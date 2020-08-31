630ce90188dc14d42fdfbf3dcce0086bef57fd2b
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-31 15:39:08 +0800

    Update function.js

--- function.js
+++ function.js
@@ -19,8 +19,14 @@ function toggleMaxBulk() {
   game.maxBulk = Math.round(ret)
 }
 
-function fib(term) {
+function fib(term) { // fibonacci sequence
+  let x = (1 + Math.sqrt(5)) / 2
+  let y = -1 / x // (1-sqrt(5))/2
+  return (x ** term - y ** term) / Math.sqrt(5)
+}
+
+function fibDecimal(term) { // fibonacci sequence, but with Decimal
   let x = new Decimal(1).add(new Decimal(5).sqrt()).div(2) // (1+sqrt(5))/2
-  let y = new Decimal(1).div(x).mul(-1)
+  let y = new Decimal(-1).div(x) // (1-sqrt(5))/2
   return x.pow(term).sub(y.pow(term)).div(new Decimal(5).sqrt())
 }
