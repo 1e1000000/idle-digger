f80747d74803fb8fd17379f87250130ef9466986
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-31 16:29:28 +0800

    Update miner.js

--- miner.js
+++ miner.js
@@ -1,9 +1,36 @@
 function getMinerInitialCost(generation) {
-  return new Decimal(10).pow(fib(generation.add(4)))
+  return new Decimal(10).pow(fib(new Decimal(generation).add(4)))
 }
 
 function getMinerCost(generation,amount) { // generation: Miner #, amount: currently Miner # bought
   if (amount.lt(1000)) return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount))
   if (amount.lt(10000)) return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(2).div(1000)))
   return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(amount.log10().sqrt()).div(1000)))
+}
+
+function buyMiner(generation) {
+  if (game.coins.gte(getMinerCost(generation, game.miner.bought[generation]))) {
+    game.coins = game.coins.sub(getMinerCost(generation, game.miner.bought[0]))
+    game.miner.bought[generation] = game.miner.bought[generation].add(1)
+  }
+}
+
+function buyMultipleMiner(generation, amount) { //amount: bulk
+  let i = new Decimal(0)
+  while (game.coins.gte(getMinerCost(generation, game.miner.bought[generation])) && i.lt(amount)) {
+    buyMiner(generation)
+    i = i.add(1)
+  }
+}
+
+function buyMaxMiner(generation) {
+  let i = new Decimal(0)
+  while (game.coins.gte(getMinerCost(generation, game.miner.bought[generation])) && i.lt(10000)) {  // hardcap at 10k to prevent performance issue
+    buyCursor()
+    i = i.add(1)
+  }
+}
+
+function maxCursor(generation) {
+  buyMultipleMiner(generation, new Decimal(game.maxBulk))
 }
