0ee6e54800b6d623213a9d1711f8e1e51a3f7262
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-08-31 17:02:15 +0800

    Update saveload.js

--- saveload.js
+++ saveload.js
@@ -114,9 +114,15 @@ function reset() {
       bought: new Decimal(0),
       power: new Decimal(1)
     },
+    miner: {
+      bought: [new Decimal(0)],
+      power: [new Decimal(1)],
+      baseEff: [new Decimal(1)], // when you buy 1 Miner, the effect
+      req: [new Decimal(50)] // first one is Miner 0, require cursor amount
+    },
     dealed: new Decimal(0),
     lastTick: Date.now(),
     mainTab: 1,
     maxBulk: 1
-  }
+  };
 }
