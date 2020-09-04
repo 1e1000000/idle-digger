03d828700ccabe83c30eac1237c51086a15e9744
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-09-04 10:52:25 +0800

    Update miner.js

--- miner.js
+++ miner.js
@@ -37,8 +37,11 @@ function maxMiner(generation) {
 
 function getTotalMinerDamage() {
   let ret = new Decimal(0)
+  let rett = new Decimal(0)
   for (let i=0; i<4; i++) {
-    ret = ret.add(game.miner.bought[i].mul(minerBaseEff[i]).mul(getMinerPower(i)))
+    rett = game.miner.bought[i].mul(minerBaseEff[i]).mul(getMinerPower(i)
+    if (i = 0 && game.cursor.amount.gte(199.999)) rett = rett.mul(10)
+    ret = ret.add(rett)
   }
   return ret
 }
