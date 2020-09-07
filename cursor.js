d042402eec0364a925c0d84bb9aa7cd6a33f3d63
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-09-07 15:49:39 +0800

    Update cursor.js

--- cursor.js
+++ cursor.js
@@ -45,7 +45,7 @@ function maxCursor(generation) {
 }
 
 function getCursorDamage() {
-  let ret = game.cursor.amount[0].mul(getCursorPower(0)).pow(getDamageExp()).add(1).div(100)
+  let ret = game.cursor.amount[0].mul(getCursorPower(0)).add(1).div(100)
   if (game.cursor.amount[0].gte(99.999)) ret = ret.add(getTotalMinerDamage().div(100))
   return ret
 }
