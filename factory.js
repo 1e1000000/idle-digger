9844ad589321feac9b412a9112f8bc9c83a25fb2
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-09-08 21:45:51 +0800

    Update factory.js

--- factory.js
+++ factory.js
@@ -1,1 +1,11 @@
+function getFactoryUpgradeCost(id,amount) {
+  if (amount.lt(100)) return factoryUpgradeInitCost[id].mul(factoryUpgradeCostScaling[id].pow(amount))
+  else return factoryUpgradeInitCost[id].mul(factoryUpgradeCostScaling[id].pow(amount.pow(2).div(100)))
+}
 
+function buyFactoryUpgrade(id) {
+  if (game.factoryEnergy.gte(getFactoryUpgradeCost(id, game.factoryUpgrade[id]))) {
+    game.factoryEnergy = game.factoryEnergy.sub(getFactoryUpgradeCost(id, game.factoryUpgrade[id]))
+    game.miner.bought[id] = game.miner.bought[id].add(1)
+  }
+}
