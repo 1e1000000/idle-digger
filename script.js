1baaa6216a7ba19419caee67c6787a158cfb2bf7
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-09-09 18:02:16 +0800

    Update script.js

--- script.js
+++ script.js
@@ -99,7 +99,7 @@ function loop(unadjusted, off = 0) { //the begin of gameloop
   document.getElementById("factoryEnergyPerSecond").innerHTML = "You are getting " + formate(getFactoryEnergyPerSecond(), 3) + " Factory Energy per second (based on Miners),"
   document.getElementById("factoryEnergyCap").innerHTML = "but will capped at " + formate(getFactoryEnergyCap(), 3) + " Factory Energy (based on Cursors)"
   for (let i=1; i<5; i++) {
-    if (i !== 3 || i !== 4) document.getElementById("factoryUpg" + i + "Eff").innerHTML = formate(getFactoryUpgEff(i), 2)
+    document.getElementById("factoryUpg" + i + "Eff").innerHTML = formate(getFactoryUpgEff(i), 2)
     if (i > 3.5) {
       document.getElementById("factoryUpg" + i + "Level").innerHTML = (game.factoryUpgrade[i].gte(0.5) ? "Purchased" : "Not Purchased")
       document.getElementById("factoryUpg" + i + "Cost").innerHTML = "Cost: " + formate(factoryUpgradeInitCost[i], 3)
