2f70f22bc8eb6f45c1845220b668781d71f6c30c
1e1000000 <54700438+1e1000000@users.noreply.github.com>
2020-09-11 21:18:28 +0800

    Update miner.js

--- miner.js
+++ miner.js
@@ -5,7 +5,9 @@ function getMinerInitialCost(generation) {
 function getMinerCost(generation,amount) { // generation: Miner #, amount: currently Miner # bought
   if (amount.lt(1000)) return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount))
   if (amount.lt(10000)) return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(2).div(1000)))
-  return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(amount.log10().sqrt()).div(1000)))
+  if (amount.lt(100000)) return getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(amount.log10().sqrt()).div(1000)))
+  let ret = getMinerInitialCost(generation).mul(new Decimal(1.09).pow(amount.pow(amount.log10().sqrt()).div(1000))).log10()
+  return new Decimal(10).pow(ret.pow(amount.div(100000)))
 }
 
 function buyMiner(generation) {
